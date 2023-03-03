01a5e62feb6330c4a4304ebdf3f36f726e2678e1
MrsEzaia <mrs.ezaia@mail.ru>
2023-03-03 08:38:23 +0300

    Initial commit

---
+++ js/services.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper("#swiper1", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  slidesOffsetAfter: 70,
+  centerSlides: true,
+});
+
+const swiper2 = new Swiper("#swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 16,
+  slidesPerView: "auto",
+  slidesOffsetAfter: 70,
+  centerSlides: true,
+});
