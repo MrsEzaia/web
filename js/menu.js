47679df76de5c4b3acbbe117954022b22de815af
MrsEzaia <mrs.ezaia@mail.ru>
2023-03-10 10:11:09 +0300

    2023/03/10

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,18 @@
-let menuBtn = document.querySelector(".icon__burger");
-let menu = document.querySelector(".menu");
-menuBtn.addEventListener("click", function () {
-  menu.classList.toggle("active");
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".item__burger");
+const closeIcon = document.querySelector(".burger__close");
+const menuIcon = document.querySelector(".btn");
 
-  menu.classList.toggle("active");
-});
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = ".icon__burger";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
