d0b35baa059ef46629a3d5be894b46f73bd57403
MrsEzaia <mrs.ezaia@mail.ru>
2023-03-10 09:45:37 +0300

    2023/03/10

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".item__burger");
+let menu = document.querySelector(".menu");
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("active");
+
+  menu.classList.toggle("active");
+});
