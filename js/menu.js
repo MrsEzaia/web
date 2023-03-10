e9eb9a92d82305fcd6223f08531c7a621c52c61c
MrsEzaia <mrs.ezaia@mail.ru>
2023-03-10 10:33:31 +0300

    2023/03/10

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,40 @@
-const menu = document.querySelector(".menu");
-const hamburger = document.querySelector(".item__burger");
-const closeIcon = document.querySelector(".burger__close");
-const menuIcon = document.querySelector(".btn");
+const hamb = document.querySelector(".icon__burger");
+const popup = document.querySelector(".menu");
+const closeBurger = document.querySelector(".icon__burger-close");
+const body = document.body;
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = ".icon__burger";
-  }
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector(".menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
 }
 
-hamburger.addEventListener("click", toggleMenu);
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(closeBurger);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick(closeBurger) {
+  popup.classList.remove("open");
+  closeBurger.classList.remove("active");
+  body.classList.remove("noscroll");
+}
